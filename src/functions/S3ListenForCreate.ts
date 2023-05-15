31cbc8c7a8659d2fc6d83c536ad2b3b52ddf51df
NewCern <lwiltshire@galaxe.com>
2023-05-15 17:33:59 -0400

    first commit

---
+++ src/functions/S3ListenForCreate.ts
@@ -0,0 +1,32 @@
+import { S3Handler } from 'aws-lambda';
+import { S3EventRecord } from 'aws-lambda/trigger/s3';
+
+export const S3ListenForCreate: S3Handler = async (event): Promise<any> => {
+  try {
+    // Process the S3 create event
+    const records: S3EventRecord[] = event.Records;
+    for (const record of records) {
+      if (record.eventName === 'ObjectCreated:Put') {
+        const bucketName: string = record.s3.bucket.name;
+        const objectKey: string = record.s3.object.key;
+
+        // Perform actions on the newly created object
+        console.log(`New object created in bucket: ${bucketName}`);
+        console.log(`Object key: ${objectKey}`);
+        // ... additional processing logic
+      }
+    }
+
+    // Return a successful response
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'S3 create event processed' }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'An error occurred' }),
+    };
+  }
+};
